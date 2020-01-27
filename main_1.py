8ae2ca0cd5e43927672159f1092b3d5b02bfd3ca
Pawel-9215 <hordyniak@gmail.com>
2020-01-27 14:47:19 +0100

    you can check ascii characters for debug

--- main_1.py
+++ main_1.py
@@ -1,1 +1,20 @@
 #hello world hey hey2
+import curses as c
+
+screen = c.initscr()
+
+win = c.newwin(20, 50, 1, 1)
+
+c.noecho()
+c.cbreak()
+screen.keypad(True)
+while True:
+        win.clear()
+        char = screen.getch() #takes input
+        if char == ord('q'):
+                break
+        if char != ord('q'):
+                win.addstr(5, 10, "".join([str(char), " character: ", chr(char)]))
+        win.refresh()
+
+screen.endwin()
