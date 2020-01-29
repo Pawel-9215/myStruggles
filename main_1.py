56b919d71e3052a16148424d4a1db8551bc6fb93
Pawel-9215 <hordyniak@gmail.com>
2020-01-29 08:49:08 +0100

    it kinda works. Bu I guess now i know what screen buffers are needed for ^^

--- main_1.py
+++ main_1.py
@@ -2,22 +2,29 @@
 import curses as c
 
 screen = c.initscr()
+counter = 1
 
 win = c.newwin(20, 50, 1, 1)
 screen.nodelay(True)
-
-c.noecho()
 c.cbreak()
+c.raw()
+c.noecho()
+
+
 screen.keypad(True)
 while True:
         
         win.clear()
+        win.addstr(2, 2, str(counter))
+        counter += 1
         char = screen.getch() #takes input
         if char == ord('q'):
                 break
         if char != ord('q') and char != -1:
                 win.addstr(5, 10, "".join([str(char), " character: ", chr(char)]))
+        
         win.refresh()
+        c.napms(30)
 
 c.endwin()
 
