4e77f54c75264de17e292d5db0c11c293c8fa3d8
Pawel-9215 <hordyniak@gmail.com>
2020-01-30 17:35:33 +0000

    controls changed to wsad and snake is basically working

--- main_1.py
+++ main_1.py
@@ -7,7 +7,7 @@ from curses import KEY_DOWN, KEY_UP, KEY_LEFT, KEY_RIGHT
 def main(screen):
         
         #function calls
-        win = curses.newwin(20, 60, 4, 4)
+        win = curses.newwin(20, 60, 0, 0)
         win.border(0)
         win.keypad(1)
 
@@ -19,12 +19,12 @@ def main(screen):
         #variables
         snake = [[4,10], [4,9], [4,8]]
         food = [10,20]
-        snake_speed = 100
+        snake_speed = 150
 
-#450 - up
-#456 - down
-#452 - left
-#454 - right
+#119 - up w
+#115 - down s
+#97 - left a
+#100 - right d
 
         key = KEY_RIGHT #by default
         opposite_key = 452
@@ -38,14 +38,23 @@ def main(screen):
                 win.clear()
                 win.border(0)
                 key = key if event == -1 else event 
-                if event not in [452, 454, 450, 456, 27] or event in [opposite_key]:
+                if event not in [119, 115, 97, 100, 27] or event in [opposite_key]:
                         key = default_key 
                 
-                opposite_key = {450:456, 456:450, 452:454, 454:452}.get(key)
+                opposite_key = {119:115, 115:119, 97:100, 100:97}.get(key)
 
-                snake.insert(0, [snake[0][0] + (key == 456 and 1) + (key == 450 and -1), snake[0][1] + (key == 452 and -1) + (key == 454 and 1)])
+                snake.insert(0, [snake[0][0] + (key == 115 and 1) + (key == 119 and -1), snake[0][1] + (key == 97 and -1) + (key == 100 and 1)])
+                snake.pop(-1)
 
-                win.addch(snake[0][0], snake[0][1], "O")
+                #Draw snake head:
+                win.addch(snake[0][0], snake[0][1], "@")
+                #draw snake body:s
+
+                for seg in snake[1:]:
+                        win.addch(seg[0], seg[1], "O")
+
+
+
                 win.addstr(0, 3, "".join([" key = ", str(key)]))
                 win.addstr(0, 16, "".join([" event = ", str(event), "  "]))
                 
