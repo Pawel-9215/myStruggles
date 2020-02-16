de26343c00b3032e23cb1e247822ccd91e7adb9b
Pawel-9215 <hordyniak@gmail.com>
2020-02-16 19:12:39 +0100

    Krwe w piach, wszystko to krew w p;iach

--- nauka.py
+++ nauka.py
@@ -3,31 +3,42 @@
 #inp1 = "3 4000"
 import math
 
-known_primes = [2, 3, 5, 7, 11, 13]
 
-def is_prime_easy(num):
 
-    if num == 1:
-        return False
-    elif num % 2 == 0:
-        return False
-    elif num % 3 == 0:
-        return False
-    elif num % 5 == 0:
-        return False
-    else:
-        return True
+known_primes = []
+
+
+
+s = """
+"""
+
 
 def is_prime_hard(num):
-
-    for i in range(2, int(math.sqrt(num))):
-        if num % i == 0:
+  
+   # Corner cases 
+    if (num <= 1) : 
+        return False
+    if (num <= 3) : 
+        return True
+   
+      # This is checked so that we can skip  
+      # middle five numbers in below loop 
+    if (num % 2 == 0 or num % 3 == 0) : 
+        return False
+   
+    i = 5
+    while(i * i <= num) : 
+        if (num % i == 0 or num % (i + 2) == 0) : 
             return False
-
+        i = i + 6
+   
     return True
 
 
 
+for i in range(0, 20):
+   if is_prime_hard(i) == True:
+      known_primes.append(i)
 
 
 
@@ -35,42 +46,66 @@ def main(inp1):
     borders = []
     inp_list = []
     global known_primes
+    new_known_primes = []
     
-    s = """
-"""
     for num in inp1.split():
         borders.append(int(num))
 
     #print(borders)
     #print(type(borders))
-    first_known_prime = 2
-    last_known_prime = 3
+    prime_suspect = borders[0]
+    known_primes_included = False
 
 
-    if borders[0] >= known_primes[0] and borders[0] <= known_primes[-1]:
-        for prime in known_primes:
-            if prime >= borders[0]:
-                first_known_prime = known_primes.index(prime)
+    while prime_suspect < known_primes[0]:
+        if is_prime_hard(prime_suspect) == True:
+            inp_list.append(str(prime_suspect))
+            new_known_primes.append(prime_suspect)
+        if prime_suspect < borders[1]:
+            prime_suspect += 1
+        else:
+            known_primes = known_primes+new_known_primes
+            return inp_list
+
+    if borders[0] >= known_primes[0] >= borders[1] or borders[0] <= known_primes[-1] <= borders[1]:
+        known_primes_included = True
+    else:
+        known_primes_included = False
+    
+    if known_primes_included == True:
+        iteration = 0
+        while known_primes[iteration] < borders[0]:
+            iteration += 1
+
+        while known_primes[iteration] < borders[1]:
+            inp_list.append(str(known_primes[iteration]))
+            if iteration < len(known_primes)-1:
+                iteration += 1
+                prime_suspect = known_primes[iteration]
+            else:
+                #inp_list.append(str(known_primes[-1]))
+                prime_suspect = known_primes[-1] + 1
                 break
 
-        while known_primes[first_known_prime] <= borders[1] and first_known_prime < (len(known_primes)-1):
-            inp_list.append(str(known_primes[first_known_prime]))
-            first_known_prime += 1
+    while prime_suspect <= borders[1]:
+        if is_prime_hard(prime_suspect) == True:
+            inp_list.append(str(prime_suspect))
+            new_known_primes.append(prime_suspect)
+        if prime_suspect < borders[1]:
+            prime_suspect += 1
+        else:
+            known_primes = known_primes+new_known_primes
+            return inp_list
 
-    elif borders[1] >= known_primes[0] and borders[1] <= known_primes[-1]:
-        for i in range(len(known_primes), 0, -1):
-            if known_primes[i] >= borders[1]:
-                last_known_prime = i
-                break
-        
-        while known_primes[last_known_prime] >= borders[0] and last_known_prime > 0:
-            inp_list.append(str(known_primes[last_known_prime]))
-            first_known_prime -= 1
+    known_primes = known_primes+new_known_primes
+    return inp_list
+
+
     
 
 
-    print(s.join(inp_list))
-    print("")
+    #print(s.join(inp_list))
+    #print("")
 
 """
     for i in range(borders[0], borders[1]+1):
@@ -93,6 +128,8 @@ how_many = int(input("How many ranges will you give me?: "))
 for i in range(1, how_many+1):
     list_inp1.append(input("enter range: "))
 for ranges in list_inp1:
-    main(ranges)
+    known_primes.sort()
+    print(s.join(main(ranges)))
+    print("")
 
             
