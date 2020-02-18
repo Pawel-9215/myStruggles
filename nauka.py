f4d497976d369c69635eeb70e3fa939a2059d391
Pawel-9215 <hordyniak@gmail.com>
2020-02-18 20:17:40 +0100

    istole most of the solution

--- nauka.py
+++ nauka.py
@@ -28,8 +28,10 @@ def main(inp1):
         table_of_primes.append(True)
     limit = floor(sqrt(borders[1])) + 1
 
-    
-    
+    n = borders[1] - borders[0] + 1
+    mark = [False]*(n+1)
+    if ( borders[0] <= 1 ):
+        borders[0] = 2;
     prime_suspect = 2
 
     while(prime_suspect**2 <= limit):
@@ -42,9 +44,30 @@ def main(inp1):
         if table_of_primes[p]:
             inp_list.append(str(p))
             known_primes.append(p)
-    print(known_primes)
+    #print(mark)
+
+    for i in range(len(known_primes)):
+        loLim = floor(borders[0]/known_primes[i]*known_primes[i])
+        if(loLim == known_primes[i]) :
+            mark[loLim-borders[0]] = False
+        if loLim < borders[0]:
+            loLim += known_primes[i]
+        if loLim == known_primes[i]:
+            loLim += known_primes[i]
+
+        for j in range(loLim, borders[1]+1, known_primes[i]):
+            mark[j-borders[0]] = True
+        
+    for i in range(borders[0], borders[1]+1):
+        if not mark[i-borders[0]]:
+            known_primes.append(i)
+            inp_list.append(str(i))
 
     #print("table after loop ", table_of_primes)
+
+
+
+    print(known_primes)
     return inp_list
 
 list_inp1 = []
